83ade9dcb0f810389a8a0cc4c590a366a2dc271b
InstituteDude <105227194+InstituteDude@users.noreply.github.com>
2022-06-15 21:39:26 +0700

    >> klik dulu dong <<
    heyho heyho dude jumpa lagi bersama saya, jadi kali ini saya mempelajari code program baru dari python yaitu bagaimana caranya kita mengcasting data. Di dalam kodingan itu berisi bermacam macam tipe data dari mulai Integer, boolean, String dan Float, Untuk lebih lengkapnya kalian bisa langsung aja nichh kodingan nya diatas. Oke see you
    *Note : Kodingan ini saya mempelajari dari Kelas terbuka di Youtube

---
+++ kalz.py
@@ -0,0 +1,43 @@
+print("<> INTEGER <>")
+data_int=20;
+print("Nilai data = ",data_int,"Type >> ",type(data_int))
+
+data_float=float(data_int)
+print("Nilai data = ",data_float,"Type >> ",type(data_float))
+data_str=str(data_int)
+print("Nilai data = ",data_str,"Type >> ",type(data_str))
+data_bool=bool(data_int)
+print("Nilai data = ",data_bool,"Type >> ",type(data_bool))
+
+print("<> FLOAT <>")
+data_float=3.70;
+print("Nilai data = ",data_float,"Type >> ",type(data_float))
+
+data_int=int(data_float)
+print("Nilai data",data_int,"Type >> ",type(data_int))
+data_str=str(data_float)
+print("Nilai data = ",data_str,"Type >> ",type(data_str))
+data_bool=bool(data_float)
+print("Nilai data = ",data_bool,"Type >> ",type(data_bool))
+
+print("<> STRING <>")
+data_str="90";
+print("Nilai data =",data_str,"Type >>  ",type(data_str))
+
+data_float=float(data_str)
+print("Nilai data = ",data_float,"Type = ",type(data_float))
+data_int=int(data_str)
+print("Nilai data = ",data_int,"Type = ",type(data_int))
+data_bool=bool(data_str)
+print("Nilai data = ",data_bool,"Type = ",type(data_bool))
+
+print("<> BOOLEAN <>")
+data_bool=True
+print("Nilai data =",data_bool,"Type >>  ",type(data_bool))
+
+data_float=float(data_bool)
+print("Nilai data = ",data_float,"Type = ",type(data_float))
+data_int=int(data_bool)
+print("Nilai data = ",data_int,"Type = ",type(data_int))
+data_str=str(data_bool)
+print("Nilai data = ",data_str,"Type = ",type(data_str))
