e12db7826e762df9ea5793352b8948fe056093b2
InstituteDude <105227194+InstituteDude@users.noreply.github.com>
2022-06-16 19:45:31 +0700

    >> About Input Python <<
    heyho dudeee, Welcome back jadi pada kesempatan kali ini saya mempelajari nichh tentang bagaimana cara kita bisa menginputkan data ke python. Emang python itu tidak seribet java ya malah lebih friendly bagi kita yang suka yang simple simple xD. Oke langsung cuss aja liat ke kodingan nya See youu
    *Note : Shoutout To Kelas terbuka di youtube karena saya mempelajari berbagai hal kodingan dan program di channel tersebut

---
+++ Input.py
@@ -0,0 +1,12 @@
+#Episode Input user
+
+
+data=input("Masukan data : ")
+print(" data = ",data,", Type data >> ",type(data))
+
+Choose=float(input("Masukan angka = "))
+Choose=int(input("Masukan angka ="))
+print(" Data = ",Choose,"Type data >> ",type(Choose))
+
+Choose_boolean=bool(int(input("Masukan Nilai Boolean = ")))
+print("Data = ",Choose_boolean,"Type data >> ",type(Choose_boolean))
