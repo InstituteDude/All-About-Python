79bd43546e85cc6c43e210aa923b5b56d5d4d225
InstituteDude <105227194+InstituteDude@users.noreply.github.com>
2022-06-17 18:47:09 +0700

    >> Operasi Aritmatika <<
    Heyho yohoho dude, kembali lagi dengan si sayah jadi kali ini tuh saya belajar tentang program python lagi ni dude nama program nya yaitu operasi aritmatika. Bagaimana caranya kita mengoprasikan aritmatika di python dan juga ada apa saja sih operasi operasi nya. Oke ya dude segitu dulu langsung cuss liat aje Kodingan nya diatas.... See youu
    *Note : Seperti biasa dipelajari dari channel kelas terbuka

---
+++ OperasiAritmatika.py
@@ -0,0 +1,32 @@
+# Operasi Aritmatika
+a=13
+b=3
+#Operasi +
+hasil =a+b
+print("Hasilnya adalah >> ",hasil)
+#Operasi -
+hasil=a-b
+print("Hasilnya adalah >> ",hasil)
+#Operasi *
+hasil=a*b
+print("Hasilnya adalah >> ",hasil)
+#Operasi /
+hasil=a/b
+print("Hasilnya adalah >> ",hasil)
+#Operasi %
+hasil=a%b
+print("Hasilnya adalah >> ",hasil)
+#Operasi eksponen (pangkat)
+hasil=a**b
+print("Hasilnya adalah >> ",hasil)
+#Operasi Floor division // (sisanya dibulatkan)
+hasil=a//b
+print("Hasilnya adalah >> ",hasil)
+#Prioritas Operasi / operation epidence
+x=9
+y=4
+z=7
+hasil=x+y-z*y/x%y**z//x
+print(x,"+",y,"-",z,"*",y,"/",x,"%",y,"**",z,"//",x," >> ",hasil)
+hasil = x+y*(z-y)
+print("Hasilnya adalah >> ",hasil)
